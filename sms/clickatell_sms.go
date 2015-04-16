d530db2a22e40bd671ae8a4712ecd2aad80139c9
Gabriel Ochsenhofer <gabs@gabs.tv>
2015-04-16 10:30:17 -0300

    latin1

--- sms/clickatell_sms.go
+++ sms/clickatell_sms.go
@@ -2,8 +2,11 @@ package sms
 
 import (
 	"bytes"
+	"code.google.com/p/go-charset/charset"
+	_ "code.google.com/p/go-charset/data"
 	"crypto/tls"
 	"errors"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -41,7 +44,22 @@ func SendSMS(user, password, apiid, phonenumber, message string) (id string, err
 		Dial:            functimeout,
 	}
 	client := &http.Client{Transport: tr}
-	resp, err := client.PostForm("https://api.clickatell.com/http/sendmsg", url.Values{"user": {user}, "password": {password}, "api_id": {apiid}, "to": {phonenumber}, "text": {message}})
+	dv0 := url.Values{"user": {user}, "password": {password}, "api_id": {apiid}, "to": {phonenumber}, "text": {message}}
+	buff := new(bytes.Buffer)
+	wc, err := charset.NewWriter("ascii", buff)
+	if err != nil {
+		fmt.Println("ERROR ascii (latin1)", err)
+		return
+	}
+	wc.Write([]byte(dv0.Encode()))
+	wc.Close()
+	r0, err := http.NewRequest("POST", "https://api.clickatell.com/http/sendmsg", buff)
+	if err != nil {
+		return
+	}
+	r0.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=ascii")
+	//resp, err := client.PostForm("https://api.clickatell.com/http/sendmsg", url.Values{"user": {user}, "password": {password}, "api_id": {apiid}, "to": {phonenumber}, "text": {message}})
+	resp, err := client.Do(r0)
 	if err != nil {
 		return
 	}
